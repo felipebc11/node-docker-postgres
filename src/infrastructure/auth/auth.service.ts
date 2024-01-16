2189908b5b6f0299e2731a53f6bfde9217361aee
Felipe Bastos <felipebc11@gmail.com>
2024-01-16 17:08:16 -0300

    feat: implement AuthService

---
+++ src/infrastructure/auth/auth.service.ts
@@ -0,0 +1,19 @@
+import * as bcrypt from 'bcryptjs';
+import * as jwt from 'jsonwebtoken';
+
+export class AuthService {
+  private static secret = process.env.JWT_SECRET ?? '';
+  private static timeToExpire = process.env.JWT_EXPIRE ?? '1h';
+
+  public static async hashPassword(password: string): Promise<string> {
+    return await bcrypt.hash(password, 10);
+  }
+
+  public static async verifyPassword(password: string, hashedPassword: string): Promise<boolean> {
+    return await bcrypt.compare(password, hashedPassword);
+  }
+
+  public static async createToken(payload: { email: string; id: string }) {
+    return jwt.sign(payload, this.secret, { expiresIn: this.timeToExpire });
+  }
+}
